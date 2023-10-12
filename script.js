7f5515b55cff8b2fa70594acac685779ea0790b7
zendayat <nmapzendaya@gmail.com>
2023-10-12 09:20:25 +0300

    changes

--- script.js
+++ script.js
@@ -9,18 +9,26 @@ const Observer = new IntersectionObserver((entries) =>{
     });
 });
 
-// const SliderL = new IntersectionObserver((entries) =>{
-//     entries.forEach((entry) => {
-//         console.log(entry)
-//         if(entry.isIntersecting){
-//             entry.target.classList.add('stop');
-//         }else{
-//             entry.target.classList.remove('stop')
-//         }
-//     });
-// });
+const Fade = new IntersectionObserver((entries) =>{
+    entries.forEach((entry) => {
+        console.log(entry)
+        if(entry.isIntersecting){
+            entry.target.classList.add('appear');
+        }else{
+            entry.target.classList.remove('appear')
+        }
+    });
+});
 
 const hiddenElements = document.querySelectorAll('.name_hidden');
-const slidingElements = document.querySelectorAll('.top_l');
-hiddenElements.forEach((el) => Observer.observe(el))
-slidingElements.forEach((el) => SliderL.observe(el))
+const appearingElements = document.querySelectorAll('.me_info');
+hiddenElements.forEach((el) => Observer.observe(el));
+appearingElements.forEach((el) => Fade.observe(el));
+
+const toggleBtn = document.querySelector('.toggle_btn');
+const toggleBtnIcon = document.querySelector('.toggle_btn i');
+const dropDownMenu = document.querySelector('.dropdown_menu');
+
+toggleBtn.onclick = function() {
+    dropDownMenu.classList.toggle('open')
+}
